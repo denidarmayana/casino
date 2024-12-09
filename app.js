f4f8dd356824b604f917078eaae1131116d90ab8
denid <denidarmayana88@gmail.com>
2024-12-10 02:49:45 +0700

    baru

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 3000;
 const route = require('./routes')
 app.use(express.json());
 
-app.use('/api',route)
+
 
 app.get('/',(req,res)=>{
     res.json({name:"Games Api",version:"1.0.0"})
 })
+app.use('/api', (req, res, next) => {
+    console.log('API route accessed');
+    next();
+}, route);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
